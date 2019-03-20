a761ef9fe5060cfe2c1318b6d1e6b933ee7a9c0d
mahadi <mahadi.nsucse@gmail.com>
2019-03-20 12:07:58 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const Wrapper = () => {
   return ( 
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
    );
 }
  
